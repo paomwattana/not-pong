53d735591cd1ea7ba6dfd0f3f33362b87f703683
paomwattana <prachnachai.mek@student.mahidol.edu>
2022-07-15 16:49:43 +0700

    added sprites and background
    haven't add one for balls yet

--- UI/StartMenu.gd
+++ UI/StartMenu.gd
@@ -1,5 +1,10 @@
 extends Control
 
 
-func _on_Button_pressed():
+
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
+
+func _on_StartButton_pressed():
 	get_tree().change_scene("res://World.tscn")
